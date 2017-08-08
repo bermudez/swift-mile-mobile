e046091196b6860037929423ba33d886185bd9c0
Yashwant Gaikawad <yashwant.gaikawad@gmail.com>
2017-08-08 20:11:07 +0530

    ..

--- app/screens/VenueInfo.js
+++ app/screens/VenueInfo.js
@@ -3,18 +3,43 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 import Swiper from 'react-native-swiper';
+import { poiClusters } from '../config/sampleMapClusters';
+import VenueImage from "../assets/POIs/calibrationdrawing.png"
+import MapView, { PROVIDER_GOOGLE} from 'react-native-maps';
 
+const POIClustersData = poiClusters;
 const IOS = Platform.OS === 'ios';
 const ANDROID = Platform.OS === 'android';
+const VenuesData = parseMarkers();
+
+function parseMarkers()
+  {
+    let markers = [];
+    console.log("polygons----");
+    console.dir(POIClustersData);
+    for (var i = POIClustersData.length - 1; i >= 0; i--) {
+      console.dir(POIClustersData[i].pois);
+      for (var j = POIClustersData[i].pois.length - 1; j >= 0; j--) {
+        markers.push(POIClustersData[i].pois[j]);
+        console.dir(POIClustersData[i].pois[j]);
+      }
+    }
+    console.log("Markers-----");
+    console.dir(markers);
+    return markers;
+  }
 
 class VenueInfo extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {};
+    this.state.VenuesData = VenuesData;
   };
 
   getVal(val){
@@ -23,15 +48,34 @@ class VenueInfo extends React.Component {
   render() {
           return (
             <Swiper style={styles.wrapper} showsButtons={true}>
-            <View style={styles.slide1}>
-              <Text style={styles.text}>Hello Swiper</Text>
-            </View>
-            <View style={styles.slide2}>
-              <Text style={styles.text}>Beautiful</Text>
-            </View>
-            <View style={styles.slide3}>
-              <Text style={styles.text}>And simple</Text>
-            </View>
+            {
+            this.state.VenuesData.map(venue => (
+              <View style={styles.slide1}>
+                <View>
+                  <Text style={styles.text}>{venue.title}</Text>
+                  <Text style={styles.text}>{venue.description}</Text>
+                  <Image 
+                    style={{
+                      height:200,
+                      width:200
+                    }}
+                    source={VenueImage}
+                    />
+
+                </View>
+
+                <View>
+                  <Text style={styles.text}>Venue Directions Map Goes here</Text>
+                </View>
+                <View>
+                  <Text style={styles.text}>Venue Offers Goes here</Text>
+                  <Text style={styles.text}>Venue Offers Goes here</Text>
+                  <Text style={styles.text}>Venue Offers Goes here</Text>
+                </View>
+
+              </View>              
+            ))}
+            
           </Swiper>
       );
     }
